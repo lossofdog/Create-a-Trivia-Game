76e8c426ba7d3f827533d647dd68f028acaa688e
Ian <iantwelve@gmail.com>
2022-07-20 19:37:35 -0700

    worked through the code and added the conversion answers to lowercase

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your Code Here
+let userName = window.prompt(`Please enter your name`)
+let userScore = 0
+let playAgain = true
+
+function playGame(){
+for (let i = 0; i<questions.length;i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer.toLowerCase() === question.correctAnswer.toLowerCase()){
+        userScore=userScore+10
+
+    }
+}
+window.alert(`your score is `+userScore)
+}
+
+while (playAgain === true){
+    playGame()
+    let userChoice = window.prompt(`do you want to play the game again? Yes or No`)
+    if(userChoice === `yes`){
+        playAgain = true
+    } else {
+        playAgain = false
+        window.alert(`thanks for playing `+userName)
+    }
+}
